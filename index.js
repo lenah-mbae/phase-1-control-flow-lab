5bd0d1a9952ed8549a90422d483ec7141cc46df6
lenahmbae <lenahmbae22@gmail.com>
2025-03-26 22:35:43 +0300

    conditions

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride<=400){
+    return 'This one is on me!';
+  } 
+  else if (ride>400 && ride<=2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (ride>2000 && ride<=2500){
+        return 'I will gladly take your thirty bucks.';
+
+  }
+  else {
+    return 'No can do.';
+}
+}
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(1500));
+console.log(scuberGreetingForFeet(2001));
+console.log(scuberGreetingForFeet(2501));
+
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('Pittsburgh'));
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('thanks for everything'));
